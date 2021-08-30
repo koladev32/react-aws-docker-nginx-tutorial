6a526e1568f5d202fb2479242e3db424f9a35e45
koladev <onaelmangabo@gmail.com>
2021-08-30 19:02:08 +0100

    - refact: format file

--- src/pages/Login.test.tsx
+++ src/pages/Login.test.tsx
@@ -8,7 +8,7 @@ import {Provider} from "react-redux";
 const renderLogin = () => {
     render(
         <Provider store={store}>
-            <Login />
+            <Login/>
         </Provider>
     )
 }
